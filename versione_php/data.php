fda5491709d4d552d0ae087389286846403197d4
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-01-11 00:50:45 +0100

    Versione PHP

---
+++ versione_php/data.php
@@ -0,0 +1,43 @@
+<?php   
+
+
+$database = [
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+        ],
+        [
+            'title' => 'Live at Wembley 86',
+            'author' => 'Queen',
+            'year' => 1992,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
+        ],
+        [
+            'title' => 'Brave new World',
+            'author' => 'Iron Maiden',
+            'year' => 2000,
+            'poster' => 'https://img.discogs.com/eRRj3lc6hXIzX7D2E_eiHj4ay7o=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-9144309-1475534412-3488.jpeg.jpg'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+        ]
+    ];
+
+ ?>
---
+++ versione_php/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.x"></script>
+    <link rel="shortcut icon" href="#">
+
+    <?php
+        include "data.php"
+    ?>
+
+</head>
+<body>
+    
+<div id="app">
+        
+
+            <?php
+
+            foreach ($database as $k => $databases) { ?>
+            <div class="container"> 
+            <div class="blocks">
+            <img src="<?php echo $databases["poster"]?>" alt="">
+            <?php
+                echo $databases["title"]. "<br>";
+                echo $databases["author"]. "<br>";
+                echo $databases["year"]. "<br>";
+             ?>
+             </div>
+             </div>
+             <?php
+            }
+
+            ?>
+            
+           
+        
+        </div>
+    </div>
+</div>
+
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
