f3f86e9d128e66b9ece78d492009ca98036b73f2
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-01-11 01:34:17 +0100

    Versione PHP2

--- versione_php/index.php
+++ versione_php/index.php
@@ -5,42 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.x"></script>
-    <link rel="shortcut icon" href="#">
 
+    <link rel="shortcut icon" href="#">
+    
     <?php
         include "data.php"
     ?>
 
+
+
 </head>
 <body>
-    
-<div id="app">
-        
-            <?php
 
-            foreach ($database as $k => $databases) { ?>
-            <div class="container"> 
-            <div class="blocks">
-            <img src="<?php echo $databases["poster"]?>" alt="">
-            <?php
-                echo $databases["title"]. "<br>";
-                echo $databases["author"]. "<br>";
-                echo $databases["year"]. "<br>";
-             ?>
-             </div>
-             </div>
-             <?php
-            }
+    <?php
 
-            ?>
-             
-</div>
+    foreach ($database as $k => $databases) { ?>
+    <div class="container"> 
+    <div class="blocks">
+    <img src="<?php echo $databases["poster"]?>" alt="">
+    <h1><?php echo $databases["title"]?></h1>
+    <p><?php echo $databases["author"]?></p>
+    <p><?php echo $databases["year"]?></p>
+        </div>
+        </div>
+        <?php
+    }
 
+    ?>
 
-
-
-    <script src="script.js"></script>
 </body>
 </html>
